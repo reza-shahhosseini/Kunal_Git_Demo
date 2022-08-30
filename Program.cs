785e1e459f8a40174d100c4081a9dcf916ad84bd
Reza Shahhosseini <54216372+reza-shahhosseini@users.noreply.github.com>
2022-08-31 00:35:29 +0430

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Client
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to VS Git extension");
-            int x = 10 + 12;
+            int x = 10 + 12; // Sasan Koushki comment
 
             Console.ReadLine();
         }
